f51be10039edb832b734d5f4f09b4e689387ffe2
Noluthando <Nonhlanhlafumba@gmail.com>
2023-12-13 01:33:05 -0800

    Nolu

---
+++ beautiful.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int m = 4;
+	int n = 2;
+	int diff = m - n;
+
+	printf("difference is %d\n", diff);
+
+	return (0);
+}
